b7961025a016ffc16873aec1cf87918d8ba15412
MCHammurabi <v.exaltare@gmail.com>
2014-11-16 18:22:16 -0700

    Photo Search Example - Unit 4 Lesson 3
    Photo Search Example

---
+++ U4-L3-A2-Photo Search ExampleTests/U4_L3_A2_Photo_Search_ExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  U4_L3_A2_Photo_Search_ExampleTests.swift
+//  U4-L3-A2-Photo Search ExampleTests
+//
+//  Created by Victor Cook on 11/16/14.
+//  Copyright (c) 2014 Victor Cook. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class U4_L3_A2_Photo_Search_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ bridge.h
@@ -0,0 +1,17 @@
+//
+//  bridge.h
+//  U4-L3-A2-Photo Search Example
+//
+//  Created by Victor Cook on 11/16/14.
+//  Copyright (c) 2014 Victor Cook. All rights reserved.
+//
+
+#ifndef U4_L3_A2_Photo_Search_Example_bridge_h
+#define U4_L3_A2_Photo_Search_Example_bridge_h
+
+
+#endif
+
+#import "AFNetworking.h"
+#import "UIKit+AFNetworking.h"
+
